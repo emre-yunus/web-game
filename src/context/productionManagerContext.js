3cf47147a149d717226eb287ceff8dcdf8ba244e
emre-yunus <r0664998@student.thomasmore.be>
2021-11-05 19:13:35 +0100

    productionManagerAmount and productionManagerEfficiency work with localstorage

--- src/context/productionManagerContext.js
+++ src/context/productionManagerContext.js
@@ -1,10 +1,19 @@
-import React, {createContext, useContext, useMemo, useState} from "react";
+import React, {createContext, useContext, useEffect, useMemo, useState} from "react";
 
 const ProductionManagerContext = createContext();
 
 export function ProductionManagerProvider(props) {
-    const [productionManagerAmount, setProductionManagerAmount] = useState(0);
-    const [productionManagerEfficiency, setProductionManagerEfficiency] = useState(1);
+    const [productionManagerAmount, setProductionManagerAmount] = useState(() => {
+        const savedProductionManagerAmount = JSON.parse(localStorage.getItem("productionManagerAmount"));
+        return savedProductionManagerAmount || 0;
+    });
+    const [productionManagerEfficiency, setProductionManagerEfficiency] = useState(() => {
+        const savedProductionManagerEfficiency = JSON.parse(localStorage.getItem("productionManagerEfficiency"));
+        return savedProductionManagerEfficiency || 1;
+    });
+
+    useEffect(() => localStorage.setItem("productionManagerAmount", JSON.stringify(productionManagerAmount)), [productionManagerAmount]);
+    useEffect(() => localStorage.setItem("productionManagerEfficiency", JSON.stringify(productionManagerEfficiency)), [productionManagerEfficiency]);
 
     const api = useMemo(() => ({
         productionManagerAmount, setProductionManagerAmount, productionManagerEfficiency, setProductionManagerEfficiency
