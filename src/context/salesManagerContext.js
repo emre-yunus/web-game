c4efe2af2427d4cb8a799c429c6c0e878e1a49ea
emre-yunus <r0664998@student.thomasmore.be>
2021-11-05 19:19:03 +0100

    salesManagerAmount and salesManagerEfficiency work with localstorage

--- src/context/salesManagerContext.js
+++ src/context/salesManagerContext.js
@@ -1,10 +1,19 @@
-import React, {createContext, useContext, useMemo, useState} from "react";
+import React, {createContext, useContext, useEffect, useMemo, useState} from "react";
 
 const SalesManagerContext = createContext();
 
 export function SalesManagerProvider(props) {
-    const [salesManagerAmount, setSalesManagerAmount] = useState(0);
-    const [salesManagerEfficiency, setSalesManagerEfficiency] = useState(1);
+    const [salesManagerAmount, setSalesManagerAmount] = useState(() => {
+        const savedSalesManagerAmount = JSON.parse(localStorage.getItem("salesManagerAmount"));
+        return savedSalesManagerAmount || 0;
+    });
+    const [salesManagerEfficiency, setSalesManagerEfficiency] = useState(() => {
+        const savedSalesManagerEfficiency = JSON.parse(localStorage.getItem("salesManagerEfficiency"));
+        return savedSalesManagerEfficiency || 1;
+    });
+
+    useEffect(() => localStorage.setItem("salesManagerAmount", JSON.stringify(salesManagerAmount)), [salesManagerAmount]);
+    useEffect(() => localStorage.setItem("salesManagerEfficiency", JSON.stringify(salesManagerEfficiency)), [salesManagerEfficiency]);
 
     const api = useMemo(() => ({
         salesManagerAmount, setSalesManagerAmount, salesManagerEfficiency, setSalesManagerEfficiency
