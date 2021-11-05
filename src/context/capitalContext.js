e6c10d991a3af3b918fc5c79f404bb0cf202a070
emre-yunus <r0664998@student.thomasmore.be>
2021-11-05 18:26:08 +0100

    1. capitalAmount and salesEfficieny work with localstorage 2. productionManagerHiring and salesManagerHiring work with localstorage

--- src/context/capitalContext.js
+++ src/context/capitalContext.js
@@ -1,10 +1,19 @@
-import {createContext, useContext, useMemo, useState} from "react";
+import {createContext, useContext, useEffect, useMemo, useState} from "react";
 
 const CapitalContext = createContext();
 
 export function CapitalProvider(props) {
-    const [capitalAmount, setCapitalAmount] = useState(0);
-    const [salesEfficiency, setSalesEfficiency] = useState(1);
+    const [capitalAmount, setCapitalAmount] = useState(() => {
+        const savedCapitalAmount = JSON.parse(localStorage.getItem("capitalAmount"));
+        return savedCapitalAmount || 0;
+    });
+    const [salesEfficiency, setSalesEfficiency] = useState(() => {
+        const savedSalesEfficiency = JSON.parse(localStorage.getItem("salesEfficiency"));
+        return savedSalesEfficiency || 1;
+    });
+
+    useEffect(() => localStorage.setItem("capitalAmount", JSON.stringify(capitalAmount)), [capitalAmount]);
+    useEffect(() => localStorage.setItem("salesEfficiency", JSON.stringify(salesEfficiency)), [salesEfficiency]);
 
     const api = useMemo(() => ({
         capitalAmount, setCapitalAmount,salesEfficiency, setSalesEfficiency
--- src/context/managerHiringContext.js
+++ src/context/managerHiringContext.js
@@ -1,10 +1,19 @@
-import React, {createContext, useContext, useMemo, useState} from "react";
+import React, {createContext, useContext, useEffect, useMemo, useState} from "react";
 
 const ManagerHiringContext = createContext();
 
 export function ManagerHiringProvider(props) {
-    const [productionManagerHiring, setProductionManagerHiring] = useState(false);
-    const [salesManagerHiring, setSalesManagerHiring] = useState(false);
+    const [productionManagerHiring, setProductionManagerHiring] = useState(() => {
+        const savedProductionManagerHiring = JSON.parse(localStorage.getItem("productionManagerHiring"));
+        return savedProductionManagerHiring || false;
+    });
+    const [salesManagerHiring, setSalesManagerHiring] = useState(() => {
+        const savedSalesManagerHiring = JSON.parse(localStorage.getItem("salesManagerHiring"));
+        return savedSalesManagerHiring || false;
+    });
+
+    useEffect(() => localStorage.setItem("productionManagerHiring", JSON.stringify(productionManagerHiring)), [productionManagerHiring]);
+    useEffect(() => localStorage.setItem("salesManagerHiring", JSON.stringify(salesManagerHiring)), [salesManagerHiring]);
 
     const api = useMemo(() => ({
         productionManagerHiring, setProductionManagerHiring, salesManagerHiring, setSalesManagerHiring
